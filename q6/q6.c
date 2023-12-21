bada899acdbbcda65bc155304faca48d620835a1
agaf2 <agaf2@cin.ufpe.br>
2023-12-21 02:40:48 -0300

    Co-authored-by: Artur Vinicius Pereira Fernandes <Arturvpf@users.noreply.github.com> Co-authored-by: Felipe Mateus Falcão Barreto <fmfbCIN@users.noreply.github.com>

---
+++ q6/q6.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/*
+TO DO:
+-Limitar as entradas em setup
+-Fazer uma função para definir como vai ser o tamnho das matrizes na multiplicação
+-Fazer as funções/threads de multi. Uso de barreira?
+*/
+
+typedef struct{
+    int index;
+    float val;
+} Pair;
+
+typedef struct{
+    Pair ** matrix;
+    int lines;     //lines = linhas
+    int columns;  //columns = colunas
+    int esplines; //esplines = linhas não nulas
+    int * espcolumns; // vetor que armazena quantos elementos não nulos tem em cada linha 
+}Matrix;
+
+Matrix defaultzin = {NULL, 0, 0, 0, NULL};
+Matrix aux = {NULL, 0, 0, 0, NULL};
+
+void espacar(int lines, int columns, Matrix * matriz){
+
+    int cont;
+    Pair aux;
+    for(int i = 0; i < lines; i++){
+        (*matriz).esplines++;                             //conta quantas linhas não nulas tem na matriz
+        (*matriz).matrix = (Pair**) realloc((*matriz).matrix, ((*matriz).esplines)*sizeof(Pair*));    //aloca uma linha a mais
+        (*matriz).matrix[i] = NULL;                                  //inicializa a linha
+        (*matriz).espcolumns = (int*) realloc((*matriz).espcolumns, ((*matriz).esplines)*sizeof(int)); //aloca um elemento a mais no vetor de colunas
+        cont = 0;                       //conta quantos elementos não nulos tem na linha
+        for(int j = 0; j < columns; j++){          
+            scanf("%f", &aux.val);      
+            if(aux.val != 0){           
+                (*matriz).matrix[i] = (Pair*) realloc((*matriz).matrix[i], (cont+1)*sizeof(Pair));  //aloca um elemento a mais na linha
+                (*matriz).matrix[i][j].index = j;                                         //atribui o indice do elemento
+                (*matriz).matrix[i][j].val = aux.val;                                     //atribui o valor do elemento
+                cont++;                                                         //incrementa o contador de elementos não nulos
+            }
+        }
+        if(cont == 0)
+            (*matriz).esplines--;                  //se a linha não tiver nenhum elemento não nulo, decrementa o contador de linhas não nulas
+        else
+            (*matriz).espcolumns[i] = cont;    //atribui o numero de elementos não nulos da linha
+    }    
+}
+
+
+void setup(Matrix * matriz, int flag){
+    if(flag == 0)       //densa
+        scanf("%d %d",&(*matriz).lines, &(*matriz).columns);
+    if(flag == 1){      // vetor denso
+        scanf("%d",&(*matriz).lines);
+        (*matriz).columns = 1;
+    }
+    if(flag == 2){} //esparça
+
+    espacar((*matriz).lines, (*matriz).columns, matriz);
+
+}
+
+
+
+
+int main(){
+    int flag;
+
+
+
+
+
+    pthread_exit(NULL);
+}
